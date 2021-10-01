4b3a874e0f577139735016e0a1ac9bf48fc16595
Danilo <daniloassis.ti@gmail.com>
2021-10-01 08:54:52 -0300

    feat(init): ༼ つ ◕_◕ ༽つ nft-token

---
+++ README.md
@@ -0,0 +1,2 @@
+# NFT Token
+[How to create a Smart Contract to Mint an NFT](https://dev.to/emanuelferreira/how-to-create-a-smart-contract-to-mint-a-nft-2bbn) by @EmanuelCampos
---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomiclabs/hardhat-waffle");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "^0.8.3",
+  },
+};
